61f8fe563741d397afe4df0c76798322e31012e3
Cweb <cwebdesign@users.noreply.github.com>
2026-03-23 11:33:15 +0100

    update new license

--- Polyray-1.9/bezier.c
+++ Polyray-1.9/bezier.c
@@ -1,16 +1,21 @@
 /* bezier.c
 
+  Polyray � MIT Licensed Revival
   Copyright (C) 1993-1996, Alexander Enzmann, All rights reserved.
+  Copyright (C) 1999-2026, Clyde Meli, All rights reserved.
 
-  This software may be used for any private and non-commercial
-  use.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the �Software�), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
 
-  You may not distribute this software, in whole or in part,
-  for any commercial purpose, without the express consent of
-  the authors.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
 
-  There is no warranty or other guarantee of fitness of this software
-  for any purpose.  It is provided solely "as is".
+THE SOFTWARE IS PROVIDED �AS IS�, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
--- Polyray-1.9/color.c
+++ Polyray-1.9/color.c
@@ -1,6 +1,22 @@
 /* color.c
 
    Provide named colors to polyray
+     Polyray � MIT Licensed Revival
+   Copyright (C) 1999-2026, Clyde Meli, All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the �Software�), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED �AS IS�, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 */
 #include <stdio.h>
 #include "defs.h"
@@ -160,7 +176,8 @@ static ColorEntry Colors1[] = {
         {"goldenrod", {.858824, .858824, .439216}},
         {"gray", {.752941, .752941, .752941}},
         {"gray05", {0.05,0.05,0.05}} ,
-        {"gray10"}, /* was greytriple(1*0.10) - see below setupcolor2*/
+        {"gray10"},
+ /* was greytriple(1*0.10) - see below setupcolor2*/
         {"gray15"},
         {"gray20"},
         {"gray25"},
