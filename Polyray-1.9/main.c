af9cd39fb6fd22f3389f3ccd88fe782c50036961
Cweb <cwebdesign@users.noreply.github.com>
2026-05-13 17:50:14 +0200

    formatting fix

--- Polyray-1.9/main.c
+++ Polyray-1.9/main.c
@@ -440,7 +440,7 @@ read_defaults(int argc, char **argv, char *outfilename,
          File_Generation_Flag = 0;
          break;
       case 'o':
-	 strncpy(outfilename, optarg1,sizeof(outfilename)-1);
+	      strncpy(outfilename, optarg1,sizeof(outfilename)-1);
          outfilename[sizeof(outfilename)-1] = '\0';
          break;
       case 'O':
