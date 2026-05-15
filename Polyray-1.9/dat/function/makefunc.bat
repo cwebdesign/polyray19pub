cba61b286698b94c4c04da920dee08058614dc00
Cweb <cwebdesign@users.noreply.github.com>
2026-05-15 16:11:17 +0200

    more sample files

---
+++ Polyray-1.9/dat/function/makefunc.bat
@@ -0,0 +1,12 @@
+rem
+rem Process the various functional surfaces - these all will take quite
+rem a bit of time.
+rem
+polyray log1.pi     -o log1.tga
+polyray lundin.pi   -o lundin.tga
+polyray noisefn.pi  -o noisefn.tga
+polyray sectorl.pi  -o sectorl.tga
+polyray sinsurf.pi  -o sinsurf.tga
+polyray sombrero.pi -o sombrero.tga
+polyray superq.pi   -o superq.tga
+polyray zonal.pi    -o zonal.tga
